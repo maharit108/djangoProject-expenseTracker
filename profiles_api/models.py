483f038bef3c4df5d7c82ac8878f9b3d58bea640
maharit108 <maha.rit108@gmail.com>
2021-02-17 20:06:51 -0600

    added django superuser

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -7,25 +7,25 @@ from django.contrib.auth.models import BaseUserManager
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
-    def create_user(self, email, full_name, password=None):
+    def create_user(self, email, name, password=None):
         """Create a new user profile"""
         if not email:
             raise ValueError('Email address required')
 
         email = self.normalize_email(email)
-        user = self.model(email=email, full_name=full_name)
+        user = self.model(email=email, name=name)
 
         user.set_password(password)
         user.save(using=self._db)
 
         return user
 
-    def create_superuser(slef, email, full_name, password):
+    def create_superuser(self, email, name, password):
         """Create a superuser with given details"""
-        user = self.create_user(email, full_name, password)
+        user = self.create_user(email, name, password)
 
-        user.issuperuser = True
-
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
@@ -34,8 +34,10 @@ class UserProfileManager(BaseUserManager):
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users profile"""
     email = models.EmailField(max_length=255, unique=True)
-    full_name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     nick_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
@@ -44,7 +46,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
 
     def get_full_name(self):
         """Get Full name of user"""
-        return self.full_name
+        return self.name
 
     def get_short_name(self):
         """Get short name of user"""
